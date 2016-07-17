2eb4789f5a7ef2330c5e9c9f0da2c92a3099ca4c
Nicolás Santángelo <santangelonicolas@gmail.com>
2016-07-17 16:47:55 -0300

    UX: Enter closes the modal, delay the highlight a bit and do it once, don't search non-numbers

--- content.js
+++ content.js
@@ -5,56 +5,74 @@
 
   var modal = {
     el: document.createElement('div'),
+    input: null,
     load: function (callback) {
       var xmlhttp = new XMLHttpRequest()
 
       xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
           modal.el.innerHTML = xmlhttp.responseText
+          modal.input = modal.el.getElementsByTagName('input')[0]
           modal.close()
           document.body.appendChild(modal.el)
 
-          // keyup on modal input
-          modal.addEventListener('input', 'keyup', function () {
-            // minidebounce
+          // Text input
+          var lineNumbers = []
+          modal.addEventListener('input', 'keyup', function (event) {
+            if (!isNumber(this.value)) {
+              return
+            }
 
-            var lineNumbers = findLineNumbers(this.value)
+            if (event.key === 'Enter' && lineNumbers.length === 1) {
+              modal.close()
+              lineNumbers = []
+              return
+            }
+
+            lineNumbers = findLineNumbers(this.value)
+
+            if (lineNumbers.length === 0) {
+              return
+            }
+
             // search result
-            if (lineNumbers.length) {
-              var number  = lineNumbers[0]
-              var sibling = number.nextElementSibling
-              scrollTo(0, number.offsetTop)
+            var number  = lineNumbers[0]
+            var sibling = number.nextElementSibling
+            scrollTo(0, number.offsetTop)
 
+            debounce(function () {
               // Don't hightlight if it didn't change
               sibling.style.backgroundColor = "#F8EEC7"
 
               setTimeout(function () {
                 sibling.style.backgroundColor = "#FFF"
               }, 450)
-            }
+            })
+
             // enter on modal input (cycle if > 1)
             // enter on modal input (cycle goto if == 1)
           })
 
-          // close button
+          // Close button
           modal.addEventListener('svg', 'click', modal.close)
 
           callback()
         }
       }
 
-      xmlhttp.open("GET", MODAL_URL, true)
+      xmlhttp.open('GET', MODAL_URL, true)
       xmlhttp.send()
     },
     toggle: function () {
       // TODO
     },
     open: function () {
-      modal.el.style.display = "block"
-      modal.el.getElementsByTagName('input')[0].focus()
+      modal.el.style.display = 'block'
+      modal.input.focus()
     },
     close: function () {
-      modal.el.style.display = "none"
+      modal.el.style.display = 'none'
+      modal.input.value = ''
     },
     addEventListener: function (selector, event, fn) {
       modal.el.querySelector(selector).addEventListener(event, fn, false)
@@ -94,6 +112,20 @@
 
     obs.observe(document.getElementById('js-pjax-loader-bar'), { attributes: true, attributeFilter: ['class'] })
     return obs
+  }
+
+  var debounce = (function debounce() {
+    var timerId
+    return function (fn, time) {
+      clearTimeout(timerId)
+      timerId = setTimeout(function() {
+        fn()
+      }, time || 180)
+    }
+  })()
+
+  function isNumber(value) {
+    return !isNaN(+value)
   }
 })()
 
